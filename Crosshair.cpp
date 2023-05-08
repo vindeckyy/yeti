1ec4b94ffbab1e4f9ccf4b95fb33902ffb6506d5
vindeckyy <35399491+vindeckyy@users.noreply.github.com>
2023-05-08 19:37:49 -0400

    Update to Crosshair.cpp
    changed sum stuff in high af probably doesn’t even work

--- Crosshair.cpp
+++ Crosshair.cpp
@@ -1,35 +1,35 @@
-#include "Crosshair.h"
+// Constants
+const float RADIUS = 30.f;
+const int X_CENTER = screen_size_x / 2;
+const int Y_CENTER = screen_size_y / 2;
 
-Crosshair crosshair;
+// Draws crosshair and penetration reticle
+void DrawCrosshairAndPenetrationReticle() {
+    // Get local player
+    auto m_local = game::localdata.localplayer();
 
-void Crosshair::paint() {
-	if (visualconfig.bSpreadCrosshair) {
-		auto m_local = game::localdata.localplayer();
-	
-		if (m_local && m_local->IsAlive()) {
-			auto m_weapon = m_local->GetWeapon();
-			if (!m_weapon || m_weapon->IsKnife()) return;
+    // Check if local player is alive and has a weapon
+    if (m_local && m_local->IsAlive()) {
+        auto m_weapon = m_local->GetWeapon();
+        if (m_weapon && !m_weapon->IsKnife()) {
+            // Draw spread crosshair
+            if (visualconfig.bSpreadCrosshair) {
+                // Calculate accuracy of the weapon
+                auto accuracy = m_weapon->GetCone() * 550.f;
 
-			int xs, ys;
-			m_pEngine->GetScreenSize(xs, ys);
-			xs /= 2; ys /= 2;
+                // Draw filled circle with the calculated accuracy
+                draw.filled_circle(X_CENTER, Y_CENTER, RADIUS, accuracy, Color(27, 27, 27, 140));
+            }
 
-			auto accuracy = m_weapon->GetCone() * 550.f;
-			draw.filled_circle(xs, ys, 30, accuracy, Color(27, 27, 27, 140));
-		}
-	}
+            // Draw penetration reticle
+            if (visualconfig.bPenetrationReticle) {
+                // Calculate damage and color based on whether the bullet can penetrate or not
+                float damage;
+                Color rect_color = trace_autowallable(damage) ? Color(130, 241, 13) : Color(255, 102, 102);
 
-	if (visualconfig.bPenetrationReticle) {
-		auto m_local = game::localdata.localplayer();
-
-		if (m_local && m_local->IsAlive()) {
-			float damage;
-
-			int xs, ys;
-			m_pEngine->GetScreenSize(xs, ys);
-			xs /= 2; ys /= 2;
-
-			draw.outlined_rect(xs - 2, ys - 2, 5, 5, Color(27, 27, 27), trace_autowallable(damage) ? Color(130, 241, 13) : Color(255, 102, 102));
-		}
-	}
+                // Draw outlined rectangle with the calculated color
+                draw.outlined_rect(X_CENTER - 2, Y_CENTER - 2, 5, 5, rect_color);
+            }
+        }
+    }
 }
